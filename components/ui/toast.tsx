0337e339dc744280f12b8865ce86323206b67dd8
Itz-Viggy <vigneshwar.sundararajan@gmail.com>
2025-05-18 00:43:17 -0400

    Toast Bg fixed

--- components/ui/toast.tsx
+++ components/ui/toast.tsx
@@ -25,11 +25,22 @@ const ToastViewport = React.forwardRef<
 ToastViewport.displayName = ToastPrimitives.Viewport.displayName;
 
 const toastVariants = cva(
-  'group pointer-events-auto relative flex w-full items-center justify-between space-x-4 overflow-hidden rounded-md border p-6 pr-8 shadow-lg transition-all data-[swipe=cancel]:translate-x-0 data-[swipe=end]:translate-x-[var(--radix-toast-swipe-end-x)] data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=move]:transition-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[swipe=end]:animate-out data-[state=closed]:fade-out-80 data-[state=closed]:slide-out-to-right-full data-[state=open]:slide-in-from-top-full data-[state=open]:sm:slide-in-from-bottom-full',
+  'group pointer-events-auto relative flex w-full items-center justify-between space-x-4 overflow-hidden rounded-md border p-6 pr-8 shadow-lg transition-all bg-[#000000] before:absolute before:inset-0 before:bg-[#000000] before:z-[-1] after:absolute after:inset-0 after:bg-[#000000] after:z-[-2] \
+data-[swipe=cancel]:translate-x-0 \
+data-[swipe=end]:translate-x-[var(--radix-toast-swipe-end-x)] \
+data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] \
+data-[swipe=move]:transition-none \
+data-[state=open]:animate-in \
+data-[state=closed]:animate-out \
+data-[swipe=end]:animate-out \
+data-[state=closed]:slide-out-to-right-full \
+data-[state=open]:slide-in-from-top-full \
+data-[state=open]:sm:slide-in-from-bottom-full',
   {
     variants: {
       variant: {
-        default: 'border bg-white dark:bg-black text-foreground',
+        default:
+          'border-0 !bg-[#000000] !text-white backdrop-blur-none before:!bg-[#000000] after:!bg-[#000000]',
         destructive:
           'destructive group border-destructive bg-destructive text-destructive-foreground',
       },
